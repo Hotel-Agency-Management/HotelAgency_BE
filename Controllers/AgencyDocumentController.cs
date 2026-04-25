d2ea2075ca0862ad90626aad1280bd866527b224
Samya Hamed <samyahamed22@gmail.com>
2026-04-25 23:02:40 +0300

    Refactor Agency Documents Files

--- Controllers/AgencyDocumentController.cs
+++ Controllers/AgencyDocumentController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Booking.Constants;
 using Booking.Filters;
+using Booking.Models;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace Booking.Controllers
@@ -11,21 +13,25 @@ namespace Booking.Controllers
     [ApiController]
     [Authorize(Roles = $"{Roles.AgencyOwner}")]
     [EnsureAgencyExistsForOwnerAttribute]
-    [Route("api/agencies/{agencyId}/documents")]
-    public class AgencyDocumentController(IAgencyDocumentService _documentService) : ControllerBase
+    [Route("api/documents")]
+    public class AgencyDocumentController(
+        IAgencyDocumentService _documentService,
+        UserManager<ApplicationUser> _userManager) : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetDocuments(int agencyId)
+        public async Task<IActionResult> GetDocuments()
         {
-            var documents = await _documentService.GetDocumentsByAgencyAsync(agencyId);
+            var agencyOwner = await _userManager.GetUserAsync(User);
+            var documents = await _documentService.GetDocumentsByAgencyAsync(agencyOwner!.AgencyId!.Value);
             return Ok(documents);
         }
 
         [HttpPost]
-        public async Task<IActionResult> UploadDocument(int agencyId, [FromForm] UploadDocumentDto dto)
+        public async Task<IActionResult> UploadDocument([FromForm] UploadDocumentDto dto)
         {
-            var result = await _documentService.UploadDocumentAsync(agencyId, dto);
-            return CreatedAtAction(nameof(GetDocuments), new { agencyId }, result);
+            var agencyOwner = await _userManager.GetUserAsync(User);
+            var result = await _documentService.UploadDocumentAsync(agencyOwner!.AgencyId!.Value, dto);
+            return CreatedAtAction(nameof(GetDocuments), new { agencyOwner!.AgencyId!.Value }, result);
         }
 
         [HttpPut("{documentId}")]
