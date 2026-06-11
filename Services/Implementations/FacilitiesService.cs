383b0e2474cf54010240e1fb76b519b3b97c0fce
Samya Hamed <samyahamed22@gmail.com>
2026-06-11 19:15:33 +0300

    remove unread parameter

--- Services/Implementations/FacilitiesService.cs
+++ Services/Implementations/FacilitiesService.cs
@@ -9,14 +9,13 @@ namespace Booking.Services
 {
     public class FacilityService(
         IFacilityRepository _facilityRepository,
-        IHotelRepository _hotelRepository,
         ISystemLogService _logService) : IFacilityService
     {
         public async Task<FacilityResponse> CreateFacilityAsync(int hotelId, CreateFacilityRequest request)
         {
 
             //var hotel = await _hotelRepository.GetByIdAsync(hotelId)
-                //?? throw new HotelNotFoundException(hotelId);
+            //?? throw new HotelNotFoundException(hotelId);
 
             if (await _facilityRepository.ExistsByNameAndHotelIdAsync(request.Name, hotelId))
                 throw new FacilityAlreadyExistsException();
@@ -50,7 +49,7 @@ namespace Booking.Services
         public async Task<IEnumerable<FacilityResponse>> GetFacilitiesByHotelIdAsync(int hotelId)
         {
             //var hotel = await _hotelRepository.GetByIdAsync(hotelId)
-                //?? throw new HotelNotFoundException(hotelId);
+            //?? throw new HotelNotFoundException(hotelId);
 
             var facilities = await _facilityRepository.GetAllByHotelIdAsync(hotelId);
             return facilities.Select(f => new FacilityResponse(f));
