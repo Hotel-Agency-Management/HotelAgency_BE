6dd3cc6c9c2b578eaf9fb5f8d4719e8293dd018b
Samya Hamed <samyahamed22@gmail.com>
2026-06-21 14:52:19 +0300

    remove comments

--- Services/Implementations/FacilitiesService.cs
+++ Services/Implementations/FacilitiesService.cs
@@ -14,10 +14,6 @@ namespace Booking.Services
     {
         public async Task<FacilityResponse> CreateFacilityAsync(int hotelId, CreateFacilityRequest request)
         {
-
-            //var hotel = await _hotelRepository.GetByIdAsync(hotelId)
-            //?? throw new HotelNotFoundException(hotelId);
-
             if (await _facilityRepository.ExistsByNameAndHotelIdAsync(request.Name, hotelId))
                 throw new FacilityAlreadyExistsException();
 
@@ -50,9 +46,6 @@ namespace Booking.Services
 
         public async Task<IEnumerable<FacilityResponse>> GetFacilitiesByHotelIdAsync(int hotelId)
         {
-            //var hotel = await _hotelRepository.GetByIdAsync(hotelId)
-            //?? throw new HotelNotFoundException(hotelId);
-
             var facilities = await _facilityRepository.GetAllByHotelIdAsync(hotelId);
             return facilities.Select(f => new FacilityResponse(f));
         }
